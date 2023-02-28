07961d92cea561dde85f01dd7a3e8e562ee128de
Davidrami12 <davidrami2000@gmail.com>
2023-02-28 19:29:11 +0100

    configuración Vuex con TypeScript

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+// My custom modules
+import exampleModule from './module-template';
+import { ExampleStateInterface } from './module-template/state';
+
+
+export interface StateInterface {
+  // Define your own store structure, using submodules if needed
+  // example: ExampleStateInterface;
+  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
+  example: ExampleStateInterface
+}
+
+export default createStore<StateInterface>({
   modules: {
+    example: exampleModule
   }
 })
