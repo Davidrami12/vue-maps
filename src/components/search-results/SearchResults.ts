3919a5be46e4eea7f6e0b8116f33480f8796ab0d
Davidrami12 <davidrami2000@gmail.com>
2023-03-08 21:41:44 +0100

    commit activar lugar y mover cámara al lugar seleccionado

--- src/components/search-results/SearchResults.ts
+++ src/components/search-results/SearchResults.ts
@@ -1,17 +1,32 @@
 import { watch, defineComponent, onMounted, ref } from 'vue';
-import { usePlacesStore } from '@/composables';
+import { usePlacesStore, useMapStore } from '@/composables';
+import { Feature } from '@/interfaces/places';
 
 
 export default defineComponent({
     name: 'SearchResults',
     setup(){
 
-        const {isLoadingPlaces, places} = usePlacesStore()
+        const { isLoadingPlaces, places } = usePlacesStore()
+        const { map } = useMapStore()
+        const activePlace = ref('')
 
 
         return{
             isLoadingPlaces,
             places,
+            activePlace,
+
+            onPlaceClicked: ( place: Feature ) => {
+                activePlace.value = place.id
+
+                const [lng, lat] = place.center
+
+                map.value?.flyTo({
+                    center: [lng, lat],
+                    zoom: 14
+                })
+            }
         }
     }
 })
--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -8,11 +8,20 @@
     </div>
 
     <ul v-else-if="places.length > 0" class="list-group mt-3">
-        <li v-for="place in places" :key="place.id" class="list-group-item list-group-item-action">
+        <li v-for="place in places" 
+            :key="place.id" 
+            @click="onPlaceClicked(place)" 
+            class="list-group-item list-group-item-action" 
+            :class="{ 'active': place.id === activePlace }">
+            
             <h5>{{ place.text }}</h5>
             <p>{{ place.place_name }}</p>
             <div align="right">
-                <button class="btn btn-outline-primary btn-sm">Direcciones</button>
+                <button 
+                    class="btn btn-outline-primary btn-sm"
+                    :class="(place.id === activePlace) ? 'btn-outline-light' : 'btn-outline-primary'">
+                    Direcciones
+                </button>
             </div>
 
         </li>
